c857d677c803bb6a17dfc7bda701ef0b225ee923
OKder <cooky73200@gmail.com>
2021-08-22 00:20:31 +0800

    hot-fix test1

--- src/com/how2java/action/TestHibernateCriteriaEx1.java
+++ src/com/how2java/action/TestHibernateCriteriaEx1.java
@@ -13,7 +13,7 @@ public class TestHibernateCriteriaEx1 {
 		session.beginTransaction();
 		
 		session.createSQLQuery("");
-		
+		System.out.println("超棒的");
 		session.getTransaction().commit();
 		
 		factory.close();
